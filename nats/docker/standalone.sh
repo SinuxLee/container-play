ffc5384794e13b7e32a9b0df8bb5cf4ff71d9bf0
sinuxlee <pingfan14@gmail.com>
2026-04-21 11:37:08 +0800

    enrich nats info

--- nats/docker/standalone.sh
+++ nats/docker/standalone.sh
@@ -2,9 +2,11 @@
 set -ueo pipefail
 
 docker run -d \
---name nats-server \
---hostname nats-server \
+--name nats \
+--hostname nats \
 --restart=always \
 -p 4222:4222 \
--v $PWD/nats/data:/data
-nats:2.12  -js
+-p 8222:8222 \
+-p 6222:6222 \
+-v $PWD/nats/data:/data \
+nats:2.12 -js -sd /data -m 8222
